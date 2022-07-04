6340db9a26d4db48daada23beef57413c6ecc9a3
Ingrid Lima <ingridlimas@hotmail.com>
2022-07-04 03:05:38 -0300

    Básico terminado

--- fun.js
+++ fun.js
@@ -15,18 +15,50 @@ let year= document.getElementById('year');
 let prevMonth= document.getElementById('month-prev');
 let nextMonth= document.getElementById('month-next');
 
+prevMonth.addEventListener('click', ()=> goPrevMonth());
+nextMonth.addEventListener('click', ()=> goNextMonth());
+
 // Asigno lo actual a cada let
 month.textContent= meses[mesActual]; //Guarda en #month el mes[actual]
 year.textContent= añoActual.toString(); //A string porque es un número
+
+writeMonth(mesActual);
+
 
 
 //Funciones
 function writeMonth(month){
 
+    // Para que se previsualicen los días del mes anterior 
+    for(let i=startDay(); i>0 ; i--){ //Va hacia atrás (del 1 al 31 etc)
+        dates.innerHTML+= `<div class="weekday opaco">${getTotalDays(mesActual-1)-(i-1)}</div>`
+    }
+
+    for(let i=1; i<=getTotalDays(month); i++){
+
+        if(i===fechaActual)
+            dates.innerHTML+= ` <div class="weekday hoy">${i}</div> `;
+        else
+            dates.innerHTML+= ` <div class="weekday">${i}</div> `;
+    }
+
+    /*for(let i=getTotalDays(mesActual); i<=getTotalDays(mesActual+1) ; i++){ //Va hacia atrás (del 1 al 31 etc)
+        dates.innerHTML+= `<div class="weekday opaco">${getTotalDays(mesActual+1)+1}</div>`
+    }
+    */
+
 }
 
 function getTotalDays(month){ //Cuántos días tiene un mes
+    if(month===-1) 
+        month=11;
 
+    if(month==0 || month==2 || month==4 || month==6 || month==7 || month==9 || month==11)
+        return 31;
+    else if (month==3 || month==5 || month==8 || month==10)
+        return 30;
+    else 
+        return esBisiesto()? 29 : 28;
 }
 
 function esBisiesto(){
@@ -42,16 +74,16 @@ function startDay(){ //Dónde empieza la semana
 }
 
 //flechas
-function goLastMonth(){
+function goPrevMonth(){
     if (mesActual !== 0)
-        mesActual++;
+        mesActual--;
     else { //Si el nro de mes es 0, entonces estamos en dic (se asigna 11) del año anterior
         mesActual=11;
         añoActual--;
     }
     setNewDate(); //?? va a crear una nueva fecha********************
-
 }
+
 function goNextMonth(){
     if (mesActual !== 11)
         mesActual++;
@@ -59,11 +91,14 @@ function goNextMonth(){
         mesActual=0;
         añoActual++;
     }
-    // setNewDate();
+    setNewDate();
 }
 
 function setNewDate(){
     hoy.getFullYear(añoActual,mesActual,fechaActual);
     month.textContent=meses[mesActual];
     year.textContent=añoActual.toString();
+
+    dates.textContent='';
+    writeMonth(mesActual);
 }
